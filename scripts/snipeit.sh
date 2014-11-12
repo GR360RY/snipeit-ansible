69e9c25319fc34f414a91c19738a90b916a7dab9
Gregory Shulov <gregs@infinidat.com>
2014-11-12 11:07:04 +0200

    Trivial changes in installation script

--- scripts/snipeit.sh
+++ scripts/snipeit.sh
@@ -4,13 +4,13 @@ sudo apt-get -y install software-properties-common python-software-properties
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get -y install git ansible
-if [ ! -d "$HOME/htpc-ansible" ]; then
-	echo "\n\033[0;32m >> Clone snipeit installation repository\033[0m"
-	git clone git://github.com/GR360RY/snipeit-ansible.git "$HOME/snipeit"
+if [ ! -d "$HOME/snipeit-ansible" ]; then
+	echo "\n\033[0;32m >> Clone snipeit-ansible installation repository\033[0m"
+	git clone git://github.com/GR360RY/snipeit-ansible.git "$HOME/snipeit-ansible"
 else
-	"\n\033[0;32m >> Snipeit installation repo is already available\033[0m"
+	"\n\033[0;32m >> Snipeit-Ansible Installation repo is already available\033[0m"
 fi
-cd "$HOME/snipeit"
+cd "$HOME/snipeit-ansible"
 echo "\n\033[0;32m >> Install Snipe-IT\033[0m"
 USER_ID=$(id -u)
 if test "$USER_ID" = "0"
